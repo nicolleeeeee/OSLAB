76888043ef4dd292da7297c5dcdeab9b87e7988a
nicolleeeeee <roconicole98@gmail.com>
2025-12-12 00:26:48 +0000

    lab1

---
+++ oslab.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+   int pid; pid = fork();
+   if (pid < 0)
+  {
+  printf("fork failed");
+  exit (1);
+  }
+   else if (pid == 0)
+  {
+   execlp("whoami", "1s", NULL);
+   exit (0);
+  }
+   else
+  {
+   printf("\n Process ID is: %d\n", getpid());
+   wait (NULL);
+   exit (0);
+  }
+}
